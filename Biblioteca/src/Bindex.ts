7224c1cfe8358d45d32a238f49247d2d20d4d8ed
alvigabriel <claudio.alves20112011@hotmail.com>
2022-07-06 07:19:10 -0300

    Biblioyteca

---
+++ Biblioteca/src/Bindex.ts
@@ -0,0 +1,57 @@
+// 07: 59 - aula 3
+
+
+interface livro {
+    numero: number;
+    titulo: string;
+    autor: string;
+    ano: number;
+    emprestado: string;
+
+}
+
+(function() {
+
+    function biblio(){
+        function ler(){}
+
+        function adicionar(livro: livro){
+            const row = document.createElement('tr');
+
+            row.innerHTML = `
+            <td>${livro.numero}</td>
+            <td>${livro.titulo}</td>
+            <td>${livro.autor}</td>
+            <td>${livro.ano}</td>
+            <td>${livro.emprestado}</td>
+            <td>
+                <button class="delete" data-numero="${livro.numero}">"Devolver"</button>
+            </td>
+            `;
+        }
+
+        function remover(){}
+
+        function salvar(){}
+
+        function render(){}
+
+        return{ ler, adicionar, remover, salvar, render };
+    }
+
+    const $ =(query: string): HTMLInputElement | null => document.querySelector(query);
+    $('.add')?.addEventListener('click', () => {
+        const numero = $('#numero')?.value;
+        const titulo = $('#titulo')?.value;
+        const autor = $('#autor')?.value;
+        const ano = $('#ano')?.value;
+        const emprestado = $('#emprestado')?.value;
+
+        if(!numero || !titulo || !autor || !ano || !emprestado){
+            alert('Os campos são obrigatórios');
+            return
+        }
+
+        biblio().adicionar({ numero: Number(), titulo, autor, ano: Number(), emprestado})
+    });
+})();
